376cf803a9b7f26a6ad018088ebdd039750c7a8f
Jannick <jannick.faber@unitybox.de>
2025-02-07 21:16:20 +0100

    chart

--- frontend/cm-frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/cm-frontend/src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { AuthService } from '../service/auth/auth.service';
+import { BarchartComponent } from "./barchart/barchart.component";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [BarchartComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
