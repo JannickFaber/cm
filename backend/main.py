e379e35dd3bb0af9f3e245022a588011de7f4356
Jannick <jannick.faber@unitybox.de>
2025-02-09 21:25:13 +0100

    cors update

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:4200"],
+    allow_origins=["http://localhost:4200", "https://cm-frontend-8d52.onrender.com/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
