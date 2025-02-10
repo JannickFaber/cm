64309c5570452a47128a72583d37e6b346530414
Jannick <jannick.faber@unitybox.de>
2025-02-10 16:37:32 +0100

    auth interceptor

--- frontend/cm-frontend/src/main.ts
+++ frontend/cm-frontend/src/main.ts
@@ -1,11 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter, Route } from '@angular/router';
 import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authGuard } from './app/guards/auth.guard';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { importProvidersFrom } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { authInterceptor } from './app/interceptor/auth.interceptor';
 
 const routes: Route[] = [
   {
@@ -23,7 +24,7 @@ const routes: Route[] = [
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([authInterceptor])),
     importProvidersFrom(BrowserAnimationsModule), provideAnimationsAsync()
   ]
 });
